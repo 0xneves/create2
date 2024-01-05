374228977fea74147d1b6dd48d506a99d3d4d015
0xneves <asguilift@hotmail.com>
2024-01-05 09:52:49 -0300

    fix: using vars for github actions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,13 +5,10 @@ dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
-  etherscan: {
-    apiKey: `${process.env.ETHERSCAN_API_KEY || ""}`,
-  },
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_RPC_URL || "",
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY || ""],
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY as string],
     },
   },
 };
