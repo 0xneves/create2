ed561ec999d960706e67356e02a85427607449da
0xneves <asguilift@hotmail.com>
2024-02-03 00:17:40 +0000

    feat: chains are managed automatically based on network selected

--- scripts/deploySalted.ts
+++ scripts/deploySalted.ts
@@ -5,14 +5,23 @@ dotenv.config();
 
 async function main() {
   const [signer] = await ethers.getSigners();
+
+  // Assert the connected network to the deployed Chef
+  let chefAddress;
+  if ((await ethers.provider.getNetwork()).chainId == 11155111) {
+    chefAddress = process.env.SEPOLIA_DEPLOYED_ADDRESS;
+  } else if ((await ethers.provider.getNetwork()).chainId == 80001) {
+    chefAddress = process.env.MUMBAI_DEPLOYED_ADDRESS;
+  }
+
   const deployer = await ethers.getContractAt(
     "Seasoning",
-    "0x97e49972DaE2AF00E9E35aA22C2c45c2eD295525",
+    chefAddress || "",
     signer,
   );
 
   const salt =
-    "0x5e06643be5cba06442ff1546d5b82b17031235a87424389321acb2975c6ebde2";
+    "0xbb51776a39ca1068b0d300e0ac75bcbd5ecaf926ea1e8bc12b728269425def73";
   const address = await deployer.callStatic.deploy(salt, bytecode);
 
   const tx = await deployer.deploy(salt, bytecode);
--- scripts/findAddress.ts
+++ scripts/findAddress.ts
@@ -7,17 +7,17 @@ async function main() {
   // If you have a faster method, please get in contact.
   console.log("Looking for addresses..");
 
+  // Assert the connected network to the deployed Chef
+  let chefAddress;
+  if ((await ethers.provider.getNetwork()).chainId == 11155111) {
+    chefAddress = process.env.SEPOLIA_DEPLOYED_ADDRESS;
+  } else if ((await ethers.provider.getNetwork()).chainId == 80001) {
+    chefAddress = process.env.MUMBAI_DEPLOYED_ADDRESS;
+  }
+
   while (true) {
     // Generate random salt
     const salt = ethers.utils.randomBytes(32);
-
-    // Assert the connected network to the deployed Chef
-    let chefAddress;
-    if ((await ethers.provider.getNetwork()).chainId == 11155111) {
-      chefAddress = process.env.SEPOLIA_DEPLOYED_ADDRESS;
-    } else if ((await ethers.provider.getNetwork()).chainId == 80001) {
-      chefAddress = process.env.MUMBAI_DEPLOYED_ADDRESS;
-    }
 
     const address = ethers.utils.getCreate2Address(
       chefAddress || "",
@@ -29,7 +29,12 @@ async function main() {
     const addressNo0x = address.slice(2);
     const lookingFor5 = addressNo0x.slice(0, 5);
 
-    if (lookingFor5 == "1F504") {
+    if (
+      lookingFor5 == "1F504" ||
+      lookingFor5 == "1f504" ||
+      lookingFor5 == "00000" ||
+      lookingFor5 == "42069"
+    ) {
       const saltHex = ethers.utils.hexlify(salt);
       console.log("Address %s with Salt:", address, saltHex);
     }
